8cbd872277fb34079b4bb0b9d4c0ac5efc248e19
mirekfoo <mirekfoo@users.noreply.github.com>
2026-05-17 21:49:45 +0200

    bump version: 0.2.2 → 0.2.3

--- src/llm_utils/__init__.py
+++ src/llm_utils/__init__.py
@@ -5,4 +5,4 @@ Upon "Build a Large Language Model (From Scratch)" by Sebastian Raschka".
 """
 
 # For acceptable version formats, see https://www.python.org/dev/peps/pep-0440/
-__version__ = "0.0.1"
+__version__ = "0.2.3"
